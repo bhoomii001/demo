f5add32d22d63cd490aabbba0784135fe6bcb0aa
Bhoomi <bhoomiahir321@gmail.conm>
2025-07-02 10:56:41 +0530

    file modified

--- program2.cs
+++ program2.cs
@@ -10,7 +10,7 @@ namespace demo
     {
         static void Main(string[] args)
         {
-            Console.Write("hiihi☺");
+            Console.Write("hellllllooooooo");
             Console.Read();
         }
     }
