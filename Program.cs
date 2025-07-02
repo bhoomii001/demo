6639760faee2d86f1295313c6092afeadb49ade3
Bhoomi <bhoomiahir321@gmail.conm>
2025-07-02 10:53:00 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace demo
+{
+    internal class Program
+    {
+      public  static void Main(string[] args)
+        {
+            Console.WriteLine("hello class");
+            Console.Write("hii");
+
+            Console.Read();
+        }
+    }
+}
---
+++ program2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    internal class program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("hiihi☺");
+            Console.Read();
+        }
+    }
+}
